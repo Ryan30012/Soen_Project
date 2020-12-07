9ef3f1c7286e92841801cdd06c415e3169977959
Ryan30012 <74203166+Ryan30012@users.noreply.github.com>
2020-12-07 17:39:48 -0500

    Add files via upload

---
+++ Website-Project/constants/config.php
@@ -0,0 +1,6 @@
+<?php
+
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', ''); //put your Xampp password 
+define('DB_NAME', 'soen_project'); //put the name of the librairies
