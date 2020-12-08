fffcec166eb6803c6eaee02d778672dc8f9dbe87
TalhaM786 <44554629+TalhaM786@users.noreply.github.com>
2020-12-07 21:54:13 -0500

    Update home.php

--- Website-Project/home.php
+++ Website-Project/home.php
@@ -308,10 +308,112 @@
 
 </html>
 
+<?php
+session_start();
+ 
+ 
+  $fname=  $_POST['fname'] ;
+  $lname=  $_POST['lname'] ;
+  $email=  $_POST['email'] ;
+  $name = "mailinglist.txt";
+  $file = fopen($name,"a");
+ 
+  $finaluser= "\n".$fname." ".$lname." ".$email;
+ 
+ fwrite($file, $finaluser);
+ fclose($file);
+   $mailtext= "Dear ".$fname." ".$lname.",<br /> <br /> You have been successfully subscribed and will receive all future updates related to the SPCA.<br /><br /> Best Regards,<br /> Montreal SPCA";
+ 
+/**
+ * PHP Template for using PHPMailer to send emails.
+ * Before sending emails using the Gmail's SMTP Server, you must make some of the security and permission level
+ * settings under your Google Account Security Settings. Please create a dummy account as you will have to put in
+ * username and password
+ * Make sure that 2-Step-Verification is disabled. Follow the link https://myaccount.google.com/security
+ * Turn ON the "Less Secure App" access at https://myaccount.google.com/u/0/lesssecureapps
+ */
+ //Import the PHPMailer class into the global namespace
+ //You don't have to modify these lines.
+ use PHPMailer\PHPMailer\PHPMailer;
+ use PHPMailer\PHPMailer\SMTP;
+
+ //SMTP needs accurate times, and the PHP time zone MUST be set
+ //This should be done in your php.ini, but this is how to do it if you don't have access to that
+ date_default_timezone_set('Etc/UTC');
+
+ require '/Soen_Project/home_Page/vendor/autoload.php';
+
+ //Create a new PHPMailer instance
+ $mail = new PHPMailer ;
+ //Tell PHPMailer to use SMTP
+ $mail->isSMTP();
+ //Enable SMTP debugging
+ // SMTP::DEBUG_OFF = off (for production use)
+ // SMTP::DEBUG_CLIENT = client messages
+ // SMTP::DEBUG_SERVER = client and server messages
+ //$mail->SMTPDebug = SMTP::DEBUG_SERVER;
+ //Set the hostname of the mail server (We will be using GMAIL)
+
+ $mail->Host = 'smtp.gmail.com';
+ //Set the SMTP port number - likely to be 25, 465 or 587
+ $mail->Port = 587;
+ $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+
+ // here is the info for the email we re using for the website
+ // email: spca.group22@gmail.com
+ // password: Website22@
+ //Whether to use SMTP authentication
+ $mail->SMTPAuth = true;
+
+ //Username to use for SMTP authentication
+ $mail->Username = 'dummyemailryan@gmail.com';
+ //Password to use for SMTP authentication
+ $mail->Password = 'dummyemailryan@gmail.com';
+ //Set who the message is to be sent from
+ $mail->setFrom('dummyemailryan@gmail.com','SPCA MONTREAL');
+ //Set an alternative reply-to address
+ //$mail->addReplyTo('replyto@example.com', 'First Last');
+ //Set who the message is to be sent to email and name
+ $mail->addAddress($email, $fname.' '.$lname);
+ //Name is optional
+ //$mail->addAddress('recepientid@domain.com');
+
+ //You may add CC and BCC
+ //$mail->addCC("recepient2id@domain.com");
+ //$mail->addBCC("recepient3id@domain.com");
+
+ $mail->isHTML(true);
+
+ //You can add attachments. Provide file path and name of the attachments
+ // $mail->addAttachment("file.txt", "File.txt");
+ //Filename is optional
+ //$mail->addAttachment("images/profile.png");
 
 
 
- <!-- // // session_start();
+ //Set the subject line
+ $mail->Subject = 'SPCA Newsletter Subscription Confirmation';
+ //Read an HTML message body from an external file, convert referenced images to embedded,
+ //convert HTML into a basic plain-text alternative body
+ $mail->Body = $mailtext;
+ //You may add plain text version using AltBody
+ //$mail->AltBody = "This is the plain text version of the email content";
+ //send the message, check for errors
+ // if (!$mail->send()) {
+ //     echo 'Mailer Error: ' . $mail->ErrorInfo;
+ // } else {
+ //     echo 'Message was sent Successfully!';
+ // }
+
+//  header('Location: home.php');
+//  exit();
+
+
+ ?>
+
+
+
+<!--  // // session_start();
 //
 //
 //
